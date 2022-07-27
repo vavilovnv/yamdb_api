fa45d35f7812700ba04690ae07f6b82432d75cc4
Дмитрий Пошехонов <toycru@ya.ru>
2022-07-27 22:31:58 +0300

    models: Review, Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Title(models.Model):
+    name = models.CharField()
+    # необходимо доопределить
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.text
